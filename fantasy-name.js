51347a4cb6c44c86d0be9c103b7a9c1a06c4179f
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2019-01-02 16:17:48 -0500

    Update fantasy-name.js

--- fantasy-name.js
+++ fantasy-name.js
@@ -1,17 +1,23 @@
 class FantasyName {
+    // initialize startup value holder via a constructor, since strings are serializable and have O(1) access when the index
+    // is known, so making a string to be accessed like an array made sense here.
     constructor() {
         this.vowels = "aeiou";
         this.consonants = "bcdfghjklmnpqrstvwxyz";
+	// I decided to use an array here over string concatenation since arrays are more flexible (for future changes), and 
+	// Array.join() seems to be a bit faster (from what I've read) than string concatenation when the client's browser is
+	// one of the newest versions of Chrome using V8.
         this.new_name = [];
     }
     
+
     clearName() {
         this.new_name = [];
     }
 
     getName(len) {
-        this.new_name = [];
-        if (typeof len !== 'Number') {
+        this.clearName();
+        if (typeof len !== 'number') {
 			throw new Error("Input must be a number")
         }
         if (!len) {
